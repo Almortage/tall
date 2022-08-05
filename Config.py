72af4284f57a93422d9ce5b31113966a6bfa3cf7
AHMED DR <110259797+alhajjim@users.noreply.github.com>
2022-08-05 03:39:53 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,22 +4,26 @@ ENVIRONMENT = bool(os.environ.get('ENVIRONMENT', False))
 
 if ENVIRONMENT:
     try:
-    API_ID = int(os.environ.get('API_ID', ""))
-    API_HASH = os.environ.get('API_HASH', "")
-    BOT_TOKEN = os.environ.get('BOT_TOKEN', "")
+        API_ID = int(os.environ.get('API_ID', 8964162))
+    except ValueError:
+        raise Exception("Your API_ID is not a valid integer.")
+    API_HASH = os.environ.get('API_HASH', None)
+    BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
     try:
-    OWNER_ID = int(os.environ.get('OWNER_ID', "5297963487")
-    DATABASE_URL = os.environ.get('DATABASE_URL', None)
+        OWNER_ID = int(os.environ.get('OWNER_ID', "5297963487"))
+    except ValueError:
+        raise Exception("Your OWNER_ID is not a valid integer.")
+    DATABASE_URL = os.environ.get('DATABASE_URL', "mongodb+srv://dragon:t.me.yy8gg@dragon.7v7baed.mongodb.net/dragon?retryWrites=true&w=majority")
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")  # Sqlalchemy dropped support for "postgres" name.
     # https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre
-    MUST_JOIN = os.environ.get('MUST_JOIN', "")
+    MUST_JOIN = os.environ.get('MUST_JOIN', None)
     if MUST_JOIN.startswith("@"):
         MUST_JOIN = MUST_JOIN.replace("@", "")
 else:
     # Fill the Values
-    API_ID = ""
-    API_HASH = ""
-    BOT_TOKEN = ""
-    DATABASE_URL = ""
-    MUST_JOIN = ""
-    OWNER_ID = ""
+    API_ID = "8964162"
+    API_HASH = "57fd71da68029890610ade3b62017472"
+    BOT_TOKEN = "5545299580:AAEUMfHOJQAl4c10kYpCNfDw1YByiTgcU04"
+    DATABASE_URL = "mongodb+srv://dragon:t.me.yy8gg@dragon.7v7baed.mongodb.net/dragon?retryWrites=true&w=majority"
+    MUST_JOIN = "YY8GG"
+    OWNER_ID = "5297963487"
