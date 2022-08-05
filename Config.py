afba513a893d3e417e205d9a03217fcd16ccda53
AHMED DR <110259797+alhajjim@users.noreply.github.com>
2022-08-05 03:23:43 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,15 +4,11 @@ ENVIRONMENT = bool(os.environ.get('ENVIRONMENT', False))
 
 if ENVIRONMENT:
     try:
-        API_ID = int(os.environ.get('API_ID', ""))
-    except ValueError:
-        raise Exception("Your API_ID is not a valid integer.")
+    API_ID = int(os.environ.get('API_ID', "")
     API_HASH = os.environ.get('API_HASH', "")
     BOT_TOKEN = os.environ.get('BOT_TOKEN', "")
     try:
-        OWNER_ID = int(os.environ.get('OWNER_ID', "5297963487"))
-    except ValueError:
-        raise Exception("Your OWNER_ID is not a valid integer.")
+    OWNER_ID = int(os.environ.get('OWNER_ID', "5297963487")
     DATABASE_URL = os.environ.get('DATABASE_URL', None)
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")  # Sqlalchemy dropped support for "postgres" name.
     # https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre
