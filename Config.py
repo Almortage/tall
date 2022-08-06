ef4c7b7ffa5b7118a8b28e50ae9016d1afb4c81e
AHMED DR <110259797+alhajjim@users.noreply.github.com>
2022-08-06 20:53:26 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,15 +1,14 @@
 import os
 
 ENVIRONMENT = bool(os.environ.get('ENVIRONMENT', False))  
-API_ID = int(os.environ.get('API_ID', None))
-API_HASH = os.environ.get('API_HASH', None)
-BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
-OWNER_ID = int(os.environ.get('OWNER_ID', None))
-DATABASE_URL = os.environ.get('DATABASE_URL', None)
+API_ID = int(os.environ.get('API_ID', ""))
+API_HASH = os.environ.get('API_HASH', "")
+BOT_TOKEN = os.environ.get('BOT_TOKEN', "")
+OWNER_ID = int(os.environ.get('OWNER_ID', ""))
+DATABASE_URL = os.environ.get('DATABASE_URL', "")
 DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
-MUST_JOIN = os.environ.get('MUST_JOIN', None)
-    if MUST_JOIN.startswith("@"):
-        MUST_JOIN = MUST_JOIN.replace("@", "")
+MUST_JOIN = os.environ.get('MUST_JOIN', "")
+
 else:
     
 API_ID = ""
