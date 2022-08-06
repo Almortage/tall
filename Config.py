c4e8e4c94fe68af7d35d48d454e2dcae4bce9cd9
AHMED DR <110259797+alhajjim@users.noreply.github.com>
2022-08-06 20:39:47 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -6,7 +6,7 @@ if ENVIRONMENT:
 API_ID = int(os.environ.get('API_ID', None))
 API_HASH = os.environ.get('API_HASH', None)
 BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
-OWNER_ID = int(os.environ.get('OWNER_ID', None)) 
+OWNER_ID = int(os.environ.get('OWNER_ID', None))
 DATABASE_URL = os.environ.get('DATABASE_URL', None)
 DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")  # Sqlalchemy dropped support for "postgres" name.
     # https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre
